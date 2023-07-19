81128c34b2d74da09ce5411c6eb29b0946245929
Anderson David <57516562+AndersonD4vid@users.noreply.github.com>
2023-07-18 21:03:50 -0300

    chore: configurando conexão com mongoDB e instalando mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import mongoose from 'mongoose';
 
 class App {
    constructor() {
       this.server = express();
+
+      // configuração com o banco de dados
+      mongoose.connect('mongodb+srv://devhouse:devhouse@cluster0.gmenpoj.mongodb.net/devhouse?retryWrites=true&w=majority');
 
       this.middlewares();
       this.routes();
